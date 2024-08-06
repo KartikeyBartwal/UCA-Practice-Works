1f22b1b8f0af5064374805dbc7dfb166c36c64b6
Kartikey Bartwal <98084657+KartikeyBartwal@users.noreply.github.com>
2024-08-06 19:36:21 +0530

    Update merge_sort.c

--- Gaurav Sir/merge_sort.c
+++ Gaurav Sir/merge_sort.c
@@ -1,82 +1,115 @@
 #include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
 
-void exch(int arr[], int i, int j)
-{
-  int temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
-}
+#define MAX_SIZE 100
 
-void merge(int arr[], int aux[], int left, int mid, int right) {
-    int i = left;
-    int j = mid+1;
+// FUNCTION TO MERGE TWO SUBARRAYS OF arr[]
+void merge(int arr[], int left, int mid, int right) {
 
-    for(int k = left; k <= right; k++) aux[k] = arr[k];
-    for(int k = left; k <= right; k++) {
-        if(i > mid) arr[k] = aux[j++];
-        else if(j > right) arr[k] = aux[i++];
-        else if(aux[i] < aux[j]) arr[k] = aux[i++];
-        else arr[k] = aux[j++]; 
+    int n1 = mid - left + 1;
+
+    int n2 = right - mid;
+
+    int L[MAX_SIZE], R[MAX_SIZE];
+
+    // COPY DATA TO TEMP ARRAYS L[] AND R[]
+    for(int i = 0; i < n1; i++) {
+
+        L[i] = arr[left + i];
+    }
+
+    for(int j = 0; j < n2; j++) {
+
+        R[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0;
+
+    int j = 0;
+
+    int k = left;
+
+    // MERGE THE TEMP ARRAYS BACK INTO arr[left..right]
+    while(i < n1 && j < n2) {
+
+        if(L[i] <= R[j]) {
+
+            arr[k] = L[i];
+
+            i++;
+        } else {
+
+            arr[k] = R[j];
+
+            j++;
+        }
+
+        k++;
+    }
+
+    // COPY THE REMAINING ELEMENTS OF L[], IF ANY
+    while(i < n1) {
+
+        arr[k] = L[i];
+
+        i++;
+
+        k++;
+    }
+
+    // COPY THE REMAINING ELEMENTS OF R[], IF ANY
+    while(j < n2) {
+
+        arr[k] = R[j];
+
+        j++;
+
+        k++;
     }
 }
 
-void merge_sort_recursive(int arr[], int aux[], int left, int right) {
-    if(left >= right) return;
+// FUNCTION TO IMPLEMENT MERGE SORT
+void mergeSort(int arr[], int left, int right) {
 
-    int mid = (left+right) / 2;
-    merge_sort_recursive(arr, aux, left, mid);
-    merge_sort_recursive(arr, aux, mid+1, right);
-    merge(arr, aux, left, mid, right);
+    if(left < right) {
+
+        // FIND THE MIDDLE POINT
+        int mid = left + (right - left) / 2;
+
+        // SORT FIRST AND SECOND HALVES
+        mergeSort(arr, left, mid);
+
+        mergeSort(arr, mid + 1, right);
+
+        // MERGE THE SORTED HALVES
+        merge(arr, left, mid, right);
+    }
 }
 
-void merge_sort(int arr[], int n)
-{
-    int* aux = (int *) malloc(n * sizeof(int));
-    merge_sort_recursive(arr, aux, 0, n-1);
-    free(aux);
-}
+int main() {
 
-void test_simple_input()
-{
-  int input[] = {5, 3, 1, 9, 7};
-  int expected_output[] = {1, 3, 5, 7, 9};
+    int n;
 
-  merge_sort(input, 5);
+    // INPUT THE SIZE OF THE ARRAY
+    scanf("%d", &n);
 
-  for(int i = 0; i < 5; i++) assert(input[i] == expected_output[i]);
-}
+    int arr[MAX_SIZE];
 
-long long time_elapsed(int n)
-{
-  int *arr = (int *) malloc(n * sizeof(int));
+    // INPUT THE ELEMENTS OF THE ARRAY
+    for(int i = 0; i < n; i++) {
 
-  srand(time(NULL));
-  for(int i = 0; i < n; i++) arr[i] = rand();
+        scanf("%d", &arr[i]);
+    }
 
-  struct timeval before;
-  gettimeofday(&before, NULL);
-  long long before_millis = before.tv_sec*1000LL + before.tv_usec/1000;
+    // CALL MERGE SORT
+    mergeSort(arr, 0, n - 1);
 
-  merge_sort(arr, n);
+    // PRINT THE SORTED ARRAY
+    for(int i = 0; i < n; i++) {
 
-  struct timeval after;
-  gettimeofday(&after, NULL);
-  long long after_millis = after.tv_sec*1000LL + after.tv_usec/1000;
+        printf("%d ", arr[i]);
+    }
 
-  return after_millis-before_millis;
-}
+    printf("\n");
 
-int main()
-{
-    test_simple_input();
-
-    long long t1 = time_elapsed(32000);
-    long long t2 = time_elapsed(64000);
-    
-    printf("%lf\n", t2*1.0/t1);
     return 0;
 }
