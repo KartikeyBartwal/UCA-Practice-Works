317234c0b6d44ea4fe2aae5f50f5a19a6273c83d
Kartikey Bartwal <98084657+KartikeyBartwal@users.noreply.github.com>
2024-08-17 10:34:51 +0530

    Create objects.js

---
+++ Web Development/Table Making/frontend/javascript/objects.js
@@ -0,0 +1,4 @@
+var obj1 = { name: "name1" }; // OBJECT LITERAL
+var obj2 = new Object({ name: "name1" }); // USING Object CONSTRUCTOR
+var obj3 = Object.assign( )
+var obj4 = Object.assign( { name : "newName" } , obj2 );
