534c913992d335d05e6abf4db171e1f59a70db24
Kartikey Bartwal <98084657+KartikeyBartwal@users.noreply.github.com>
2024-09-04 20:09:23 +0530

    Create revision_types_of_function_declarations.js

---
+++ Revision Series /revision_types_of_function_declarations.js
@@ -0,0 +1,32 @@
+var name = " some name ";
+
+// TYPE 1 : NAMED FUNCTION DECLARATION 
+function foo1( param1 ) {
+
+  
+}
+
+
+// TYPE 2: FUNCTION EXPRESSION WITH VAR
+var foo2 = function ( param1 ) {
+
+  
+};
+
+// TYPE 3: ARROW FUNCTION 
+var foo3 = ( param1 ) => {
+
+  
+}
+
+// TYPE 4: ARROW FUNCTION WITHOUT VARIABLE ASSIGNMENT
+( param1 ) => {
+
+  
+}
+
+// TYPE 5: IIFE ( IMMEDIATELY INVOKED FUNCTION EXPRESSION ) WITH ARROW FUNCTION
+( (param1 )) => {
+
+  
+}();
